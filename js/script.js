1a7bc1362dc0d572d21df67b952ea3bb1d0aa9ce
brenoiscde <breninhoponticelli@gmail.com>
2024-09-15 22:06:01 -0300

    enviando para o repositorio

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const scrollToTopBtn = document.getElementById('botaoSubir');
+    
+    window.addEventListener('scroll', ()=> {
+        if (window.scrollY > 300) {
+            scrollToTopBtn.style.display = 'block';
+        } else {
+            scrollToTopBtn.style.display = 'none';
+        }
+    });
+    window.addEventListener('scroll', ()=> {
+        if (window.scrollY > 300) { // Mostra o botão após rolar 300 pixels
+            scrollToTopBtn.classList.add('show');
+            
+        } else {
+            scrollToTopBtn.classList.remove('show');
+        }
+    });
+
+    scrollToTopBtn.addEventListener('click', ()=>{
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
