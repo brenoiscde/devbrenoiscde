069008b23419f33e087bfa97de1cea242478be8f
brenoiscde <breninhoponticelli@gmail.com>
2024-11-25 13:21:50 -0300

    Trabalhando responsividade

--- js/script.js
+++ js/script.js
@@ -25,3 +25,11 @@ document.addEventListener('DOMContentLoaded', ()=> {
     });
 });
 
+function menuShow(){
+    let menuMobile = document.querySelector(".mobile-menu");
+    if (menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open');
+    }else{
+        menuMobile.classList.add('open');
+    }
+}
